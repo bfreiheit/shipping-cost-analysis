119465986d20a92c44a378d0a779c5529a7b1589
bfreiheit <belinda.freiheit@gmail.com>
2025-07-20 14:51:51 +0200

    modify function for create tables

--- src/shipping_cost_analysis/etl/db_tools.py
+++ src/shipping_cost_analysis/etl/db_tools.py
@@ -1,5 +1,10 @@
 import os
 
+from sqlalchemy import inspect
+from sqlalchemy.exc import SQLAlchemyError
+from typing import List
+import traceback
+
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.engine import Engine
@@ -18,3 +23,35 @@ def get_engine() -> "Engine":
     )
 
     return create_engine(conn_str)
+
+
+def recreate_tables(table_classes: List) -> None:
+    """
+    Deletes and creates tables based on SQLAlchemy-schemas.
+    
+    Args:
+        table_classes (List): list of SQLAlchemy-schema (e.g. [Customer, Product]).
+    """
+    engine = get_engine()
+    with engine.connect() as conn:
+        inspector = inspect(conn)
+
+        for table in table_classes:
+            try:
+                table_name = getattr(table, "__tablename__", None)
+                table_obj = getattr(table, "__table__", None)
+
+                if table_name is None or table_obj is None:
+                    raise AttributeError(f"invalid table class: {table}")
+
+                if inspector.has_table(table_name):
+                    print(f"Delete table: {table_name}")
+                    table_obj.drop(bind=conn)
+
+                print(f"Create table: {table_name}")
+                table_obj.create(bind=conn)
+
+            except (AttributeError, SQLAlchemyError) as e:
+                print(f"Error for table {table}: {e}")
+                traceback.print_exc()
+
