57d6b37a080fbcf0c239cc9fad265e09e2a3ac16
bfreiheit <belinda.freiheit@gmail.com>
2025-07-20 20:07:50 +0200

    fix db functions

--- src/shipping_cost_analysis/etl/db_tools.py
+++ src/shipping_cost_analysis/etl/db_tools.py
@@ -1,11 +1,10 @@
 import os
-import traceback
-from typing import List
 
 from dotenv import load_dotenv
-from sqlalchemy import create_engine, inspect
+from sqlalchemy import create_engine
 from sqlalchemy.engine import Engine
-from sqlalchemy.exc import SQLAlchemyError
+
+from shipping_cost_analysis.models.schema import Base
 
 load_dotenv(override=True)
 
@@ -23,32 +22,17 @@ def get_engine() -> "Engine":
     return create_engine(conn_str)
 
 
-def recreate_tables(table_classes: List) -> None:
-    """
-    Deletes and creates tables based on SQLAlchemy-schemas.
-
-    Args:
-        table_classes (List): list of SQLAlchemy-schema (e.g. [Customer, Product]).
-    """
+def drop_all_tables():
     engine = get_engine()
-    with engine.connect() as conn:
-        inspector = inspect(conn)
+    print("Dropping all tables...")
+    Base.metadata.drop_all(bind=engine)
+    print("All tables dropped.")
 
-        for table in table_classes:
-            try:
-                table_name = getattr(table, "__tablename__", None)
-                table_obj = getattr(table, "__table__", None)
+def create_all_tables():
+    engine = get_engine()
+    print("Creating all tables...")
+    Base.metadata.create_all(bind=engine)
+    print("All tables created.")
 
-                if table_name is None or table_obj is None:
-                    raise AttributeError(f"invalid table class: {table}")
 
-                if inspector.has_table(table_name):
-                    print(f"Delete table: {table_name}")
-                    table_obj.drop(bind=conn)
 
-                print(f"Create table: {table_name}")
-                table_obj.create(bind=conn)
-
-            except (AttributeError, SQLAlchemyError) as e:
-                print(f"Error for table {table}: {e}")
-                traceback.print_exc()
